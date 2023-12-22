458841ee906b9a8a5967f5323ebb4e3cd9ef0ebf
shenao-zhang <82402331+shenao-zhang@users.noreply.github.com>
2023-12-22 15:03:00 -0600

    stack once

--- run_script.sh
+++ run_script.sh
@@ -7,11 +7,7 @@ script_full_path="scripts.$script_name"
 logs_dir="logs"
 log_file_name=${script_name//./_}
 log_file="$logs_dir/$log_file_name.log"
-mkdir -p $logs_dir
+#mkdir -p $logs_dir
 
-echo "Starting $script_full_path"
 
-nohup python -u -m $script_full_path > $log_file 2>&1 &
-
-echo "Watch the log file with:"
-echo "wt $log_file"
+python -u -m $script_full_path #> $log_file 2>&1 &
