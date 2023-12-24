c6ec17b1dfb73a663a76872f223924bbc3a1df9b
shenao-zhang <82402331+shenao-zhang@users.noreply.github.com>
2023-12-23 22:47:41 -0600

    llama2

--- core/experiments_config.py
+++ core/experiments_config.py
@@ -25,7 +25,7 @@ TASKS_TO_EVALUATE = [
 ]
 
 #MODELS_TO_EVALUATE = [("pythia", "2.8B"), ("gpt-j", "6B"), ("pythia", "6.9B")]
-MODELS_TO_EVALUATE = [("pythia", "6.9B"), ("gpt-j", "6B")]
+MODELS_TO_EVALUATE = [("gpt-j", "6B")]
 MODELS_TO_EVALUATE_A = [
     # ("gpt-2", "1.5B"),
     ("pythia", "2.8B"),
