7ba40fd1bf32633c053df7f3eecb975118896779
shenao-zhang <82402331+shenao-zhang@users.noreply.github.com>
2023-12-26 15:36:46 -0600

    debug

--- core/task_vectors.py
+++ core/task_vectors.py
@@ -211,7 +211,6 @@ def get_single_context_task_hiddens(
 
     # TODO: replace traced forward with a regular forward and rely on huggingface's saved hidden states
     outputs, forward_trace = traced_forward(model, inputs=inputs)
-    print("get_single_context_task_hiddens", decode_predictions(outputs, tokenizer))
     task_hiddens = forward_trace.residual_stream.hidden[:, :, -1, :]
     _, num_layers, hidden_size = task_hiddens.shape
     task_hiddens = task_hiddens.view(len(datasets), num_test_inputs_to_avg, num_layers, hidden_size).mean(dim=1)
